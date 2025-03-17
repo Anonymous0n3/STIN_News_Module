7fe67294268d2b0d199f12a71fdc44de4c8362d5
vojtech.zmolik <anonymous.escaper@gmail.com>
2025-03-17 19:41:04 +0100

    Load of .env file done

---
+++ STIN_News_Module/Logic/EnvLoad.cs
@@ -0,0 +1,26 @@
+﻿namespace STIN_News_Module.Logic
+{
+    public class EnvLoad
+    {
+        public static void Load()
+        {
+            var envFile = Path.Combine(Directory.GetCurrentDirectory(), ".env");
+            if (!File.Exists(envFile))
+            {
+                return;
+            }
+            var lines = File.ReadAllLines(envFile);
+            foreach (var line in lines)
+            {
+                var parts = line.Split("=");
+                if (parts.Length != 2)
+                {
+                    continue;
+                }
+                var key = parts[0].Trim();
+                var value = parts[1].Trim();
+                Environment.SetEnvironmentVariable(key, value);
+            }
+        }
+    }
+}
--- STIN_News_Module/Program.cs
+++ STIN_News_Module/Program.cs
@@ -1,3 +1,5 @@
+using STIN_News_Module.Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.

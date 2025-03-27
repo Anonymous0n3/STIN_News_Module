b377fdb58368e6605fa293754755c67c23d33af2
vojtech.zmolik <anonymous.escaper@gmail.com>
2025-03-27 10:17:42 +0100

    Made JSON model to do stuff with filters

---
+++ STIN_News_Module/Logic/Filtering/FilterBase.cs
@@ -0,0 +1,9 @@
+﻿namespace STIN_News_Module.Logic.Filtering
+{
+    abstract class FilterBase
+    {
+
+        public abstract void Execute();
+
+    }
+}
---
+++ STIN_News_Module/Logic/JsonModel/DataModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace STIN_News_Module.Logic.JsonModel
+{
+    public class DataModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("date")]
+        public int Date { get; set; }
+
+        [JsonPropertyName("rating")]
+        public int Rating { get; set; }
+
+        [JsonPropertyName("sale")]
+        public bool Sale { get; set; }
+    }
+}

a0b3b8a92961eff1f6463c70006e1c226b59e563
jakub.kraus <jakub.kraus007@seznam.cz>
2025-05-14 18:47:35 +0200

    update index page

--- STIN_News_Module/Pages/Index.cshtml.cs
+++ STIN_News_Module/Pages/Index.cshtml.cs
@@ -1,19 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
 
-namespace STIN_News_Module.Pages;
-
-public class IndexModel : PageModel
+namespace STIN_News_Module.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class UserDashboardModel : PageModel
     {
-        _logger = logger;
-    }
+        public List<StockItem> Stocks { get; set; }
 
-    public void OnGet()
-    {
+        public void OnGet()
+        {
+            // Zde můžeš napojit reálná data z API, databáze nebo souboru
+            Stocks = new List<StockItem>
+            {
+                new StockItem { Name = "Microsoft", Date = DateTime.Now.AddDays(-1), Rating = 8, Sell = 0 },
+                new StockItem { Name = "Google", Date = DateTime.Now.AddDays(-2), Rating = -3, Sell = 1 },
+                new StockItem { Name = "OpenAI", Date = DateTime.Now, Rating = 5, Sell = 0 },
+            };
+        }
 
+        public class StockItem
+        {
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+            public int Rating { get; set; }
+            public int Sell { get; set; }
+        }
     }
 }

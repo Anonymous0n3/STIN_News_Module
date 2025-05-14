bdfd7cfcf4835c9e146bdcc6dfc0e24387f3bf30
jakub.kraus <jakub.kraus007@seznam.cz>
2025-05-14 19:17:06 +0200

    add button in index page

--- STIN_News_Module/Pages/Index.cshtml.cs
+++ STIN_News_Module/Pages/Index.cshtml.cs
@@ -19,6 +19,19 @@ namespace STIN_News_Module.Pages
             };
         }
 
+        public void OnPostAdd()
+        {
+            // Zde můžeš např. přidat novou akcii (staticky nebo dynamicky)
+            Stocks = new List<StockItem>
+            {
+                new StockItem { Name = "Microsoft", Date = DateTime.Now.AddDays(-1), Rating = 8, Sell = 0 },
+                new StockItem { Name = "Google", Date = DateTime.Now.AddDays(-2), Rating = -3, Sell = 1 },
+                new StockItem { Name = "OpenAI", Date = DateTime.Now, Rating = 5, Sell = 0 },
+                new StockItem { Name = "Nová akcie", Date = DateTime.Now, Rating = 1, Sell = 0 }
+            };
+        }
+
+
         public class StockItem
         {
             public string Name { get; set; }
